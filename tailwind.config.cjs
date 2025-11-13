ccf9f44c363673a6be672203e488776491e63d29
hedikaa78-del <hedikaa78@gmail.com>
2025-11-13 19:41:17 +0330

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'meta-cyan': '#06b6d4',
+        'meta-indigo': '#6366f1',
+      }
+    }
+  },
+  plugins: [],
+}
